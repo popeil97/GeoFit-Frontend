ebbfb887836451697f816d998380f37c1ddd0b9b
Nathan Cinnamond <njcinnamond@gmail.com>
2020-07-20 19:21:40 +0100

    fixed env urls

--- src/app/stravauth/stravauth.service.ts
+++ src/app/stravauth/stravauth.service.ts
@@ -20,7 +20,7 @@ export class StravauthService {
   // Uses http.post() to register user from REST API endpoint
   public authenticate() {
     //window.location.href='https://app.strava.com/oauth/authorize?client_id=49168&redirect_uri=http://localhost:8000/strava-login/&response_type=code&approval_prompt=auto&scope=write/';
-    this.http.get(environment.apiUrl + 'api/strava-login/').subscribe(
+    this.http.get(environment.apiUrl + '/api/strava-login/').subscribe(
       data => {
         if (data.hasOwnProperty('url')){
             window.location.href=data['url'];
@@ -33,10 +33,10 @@ export class StravauthService {
   }
 
   public getStravaInfo() {
-    return this.http.post(environment.apiUrl + 'api/strava-status/', {}).toPromise();
+    return this.http.post(environment.apiUrl + '/api/strava-status/', {}).toPromise();
   }
 
   public deauthorize() {
-    return this.http.post(environment.apiUrl + 'api/strava-deauth/', {}).toPromise();
+    return this.http.post(environment.apiUrl + '/api/strava-deauth/', {}).toPromise();
   }
 }
