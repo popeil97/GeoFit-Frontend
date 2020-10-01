d85a855249cbcaecf2ba6ae417f363a4e614b0de
Katie <kd459@cornell.edu>
2020-10-01 08:27:05 -0500

    map settings modal

--- src/app/race-view/race-view.component.ts
+++ src/app/race-view/race-view.component.ts
@@ -125,9 +125,8 @@ export class RaceViewComponent implements OnInit {
   }
 
   openModal(id: string) {
-    console.log("DATA SENT TO CHILD", this.progress.distance_type, this.raceID);
-    var data = (id=='raceSettingsModal') ? {userSettings:this.userRaceSettings,callbackFunction:null}:(id == 'custom-modal-5') ? {raceType:this.raceType, distance_unit: this.progress.distance_type, race_id:this.raceID, numActivities : this.num_activities, manualEntry:this.raceSettings.isManualEntry, automaticImport: this.userRaceSettings.isAutomaticImport, callbackFunction:null} : {};
-    console.log("RACE VIEW USER RACE SETTINGS", this.userRaceSettings);
+    var data = (id=='mapSettingsModal') ? {userSettings:this.userRaceSettings,callbackFunction:null}:(id == 'custom-modal-5') ? {raceType:this.raceType, distance_unit: this.progress.distance_type, race_id:this.raceID, numActivities : this.num_activities, manualEntry:this.raceSettings.isManualEntry, automaticImport: this.userRaceSettings.isAutomaticImport, callbackFunction:null} : {};
+    console.log("DATA SENT TO CHILD", data);
     data.callbackFunction = this.uploadActivity;
 
     this.modalService.open(id,data);
@@ -136,7 +135,7 @@ export class RaceViewComponent implements OnInit {
   uploadActivity = (incomingData = null) => {
   //  const toAlert = (incomingData != null) ? incomingData : this.testString;
   if(incomingData != null){
-    console.log("PARENT",incomingData.type);
+    console.log("PARENT INCOMING DATA",incomingData.type);
 
     if(incomingData.type == "manual")
     {
@@ -146,6 +145,12 @@ export class RaceViewComponent implements OnInit {
     if(incomingData.type == "strava")
     {
       this.refreshStatComponents();
+    }
+
+    if(incomingData.type == "map")
+    {
+      console.log("MAP DATA INCOMING!")
+      this.showPinsFromSettings(incomingData.pinSettings);
     }
   }
     
