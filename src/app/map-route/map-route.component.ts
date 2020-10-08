38f5dcbdd3a4d36539ddb30755cc1489d949c988
Katie <kd459@cornell.edu>
2020-10-08 18:15:47 -0500

    just small fixes across the board: activity menu, leaderboard styling, map routes, and user stats

--- src/app/map-route/map-route.component.ts
+++ src/app/map-route/map-route.component.ts
@@ -170,18 +170,28 @@ export class MapRouteComponent implements OnChanges {
       console.log("ZOOM ZOOM IN MAP");
       this.map.setZoom(9);
     }
+    else
+    {
+      this.map.setZoom(2);
+    }
     
-    
-    var color = "#36343C";
+    var color = "#7FCC92";
 
     //Add each path to map independently
     _.forEach(temp_routes_flipped,(route) => {
       this.line = L.polyline(route,{
-        color: color,
+        color: '#578a63',
         weight: 8,
         opacity: 0.8
       }).addTo(this.map);
+
+       L.polyline(route,{
+        color: color,
+        weight: 5,
+        opacity: 1
+      }).addTo(this.map);
     });
+
 
     //We store coordinates in their original order 
     //so we can work out user progression along route later
@@ -532,6 +542,11 @@ export class MapRouteComponent implements OnChanges {
 
   public createPin(userData: UserData, isTag: boolean=false){
     var img_html = "<img src=\"" + userData.profile_url + "\";\"><div class=\"pin\"></div><div class=\"pulse\"></div>";
+
+/**
+    if(userData.isMe){
+      var img_html = "<img src=\"" + userData.profile_url + "\";\"><div class=\"pin\"></div><div class=\"pulse\"></div>";
+    } **/
 
     
     var userIcon = L.divIcon({
