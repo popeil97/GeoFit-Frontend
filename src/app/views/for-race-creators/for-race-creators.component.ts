fb590c70de2aed69a4f455bc6b37e6ab0b5ab9e6
Ryan Kim <kimryan0416@gmail.com>
2021-01-13 06:15:01 -0500

    some icons provided by Katie were PNG-ified, Landing page cleaned up, scaffolding set up for 'For Race Creators' Page

--- src/app/views/for-race-creators/for-race-creators.component.ts
+++ src/app/views/for-race-creators/for-race-creators.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../auth.service';
+
+import { ModalService } from '../../modalServices';
 
 @Component({
   selector: 'app-for-race-creators',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForRaceCreatorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _authService: AuthService, private modalService: ModalService) { }
 
   ngOnInit() {
+    //If we already store a JWT locally, set it in memory
+    if (localStorage.getItem('access_token')){
+      this._authService.token = localStorage.getItem('access_token');
+    }
   }
+
+  testFunction = (incomingData = null) => {
+    const toAlert = (incomingData != null) ? incomingData : this.testString;
+    console.log(toAlert);
+  }
+
+  logout() {
+    this._authService.logout();
+  }
+
+  openModal(id: string) {
+    var data = (id == 'custom-modal-3') ? {race_id:'HELLO',callbackFunction:null} : {};
+    data.callbackFunction = this.testFunction;
+    this.modalService.open(id,data);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+}
 
 }
