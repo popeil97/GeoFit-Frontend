fb5955c29d7a235b458bde0aac1eb06a26645f03
Ryan Kim <kimryan0416@gmail.com>
2021-03-03 14:15:50 -0500

    scaffolding changes to race dashboard

--- src/app/race-dashboard/race-dashboard.component.ts
+++ src/app/race-dashboard/race-dashboard.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { RaceService } from '../race.service';
 import { RaceSettings } from '../race-about-page/race-about-page.component';
+import { AuthService } from '../auth.service';
+
+import { MatDialog } from '@angular/material';
+import { LoginComponent } from '../login/login.component';
+import { Register2Component } from '../register2/register2.component';
 
 @Component({
   selector: 'app-race-dashboard',
@@ -9,8 +14,18 @@ import { RaceSettings } from '../race-about-page/race-about-page.component';
   styleUrls: ['./race-dashboard.component.css']
 })
 export class RaceDashboardComponent implements OnInit, OnChanges {
+
+  public loading:Boolean = true;
+  public raceID: number = null;
+  public raceData: any = null;
+
+
+
+  public isOwnerOrMod:Boolean = false;
+
+  public page: string;
+
   public raceName: string;
-  public raceID: number;
 
   //Child race IDs (same as parent if no child IDs)
   public raceIDs:number[] = [];
@@ -22,20 +37,57 @@ export class RaceDashboardComponent implements OnInit, OnChanges {
   //Info of all child races if present (else just parent race)
   public childRaceData: ChildRaceData[] = [];
 
-  public loading:Boolean = false;
-
   constructor(
     private route: ActivatedRoute,
     private raceService: RaceService,
+
+    private _authService:AuthService,
+    private router:Router,
+    private dialog:MatDialog,
   ) { }
 
   ngOnInit() {
+
+    // We add a subscription to the login event
+    this._authService.getLoginStatus.subscribe(this.handleLoginChange);
+    this.loading = true;
+    this.raceData = null;
+
+    this.route.paramMap.subscribe(params => {
+      // Get the race ID from the URL
+      this.raceID = params['params']['id'];
+      if (this.raceID) {
+        // there was a race ID in the URL
+        if (this._authService.isLoggedIn()) {
+          // We are logged in
+          this.getRaceData();
+        } else {
+          // We aren't logged in
+          this.loading = false;
+        }
+      } else {
+        // There was no race ID in the url
+        this.loading = false;
+      }
+    });
+    /*
     this.loading = true;
     this.route.paramMap.subscribe(params => {
       this.raceName = params['params']['name'];
       this.raceID = params['params']['id'];
-      this.getRaceData();
+      this.page = (params['params']['page']) ? params['params']['page'] : 'admin';
+
+      //this.loadingSegments.navigation = false;
+      if (this._authService.isLoggedIn()) {
+        // We ARE logged in, so we gotta pull race data
+        this.getRaceData();
+      } else {
+        // We aren't logged in - this will auto-default to the login/signup screen in the html
+        // once they log in though, we'll need to prompt them to reload the data
+        this.loading = false;
+      }
     });
+    */
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -52,17 +104,71 @@ export class RaceDashboardComponent implements OnInit, OnChanges {
     }
   }
 
+  openLogin = () => {
+    let d = this.dialog.open(LoginComponent,{
+      panelClass:"LoginContainer",
+      data:{register:false}
+    });
+    d.afterClosed().subscribe(result=>{
+      console.log("CLOSE LOGIN FROM to_race_creators", result);
+    })
+  }
+  openRegister = () => {
+    let d = this.dialog.open(Register2Component, {
+      panelClass:"RegisterContainer",
+      data:{register:false},
+    });
+    d.afterClosed().subscribe(result=>{
+      console.log("CLOSE REGISTER FROM to_race_creators", result);
+    })
+  }
+  navigateTo(url:string = null) {
+    if (url != null) this.router.navigate([url]);
+  }
+
+  handleLoginChange = (loggedIn:Boolean) => {
+    if (loggedIn) {
+      // we've logged in
+      if (this.raceID) {
+        // race ID was provided, gotta grab data
+        this.loading = true;
+        this.getRaceData();
+      } else {
+        // No Race ID was provided
+        this.loading = false;
+        this.raceData = null;
+      }
+    } else {
+      // We've logged out
+      this.loading = false;
+      this.raceData = null;
+    }
+  }
+  reloadData() {
+    this.getRaceData();
+  }
   private getRaceData(){
     this.loading = true;
 
     this.raceService.getRace(this.raceID).subscribe((data) => {
-      let raceData = data as RaceData;
 
-      console.log("Race data: ", raceData);
+      let d = data as RaceData;
+      
+      // Catch if we're not a mod or owner
+      if (!d.is_mod_or_owner) {
+        this.loading = false;
+        this.raceData = null;
+        this.isOwnerOrMod = false;
+        return;
+      }
+      console.log("Race data: ", d);
+
+      this.raceData = d;
+      this.isOwnerOrMod = this.raceData.is_mod_or_owner;
 
       // Child race data
-      this.raceIDs = raceData.race_IDs;
-      this.childRaceData = raceData.child_race_dict;
+      this.raceIDs = this.raceData.race_IDs;
+      this.childRaceData = this.raceData.child_race_dict;
       this.childRaceData.unshift({id:this.raceID,name:'All'});
       
       // Race-specific info
