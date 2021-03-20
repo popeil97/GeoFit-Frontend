529885e7b0d0bffe814c637b4e61837e046bee84
Ryan Kim <kimryan0416@gmail.com>
2021-03-20 19:12:10 -0400

    modified Report form into radio buttons instead of checkboxes + cleaned up report form TS logic

--- src/app/popups/report-form/report-form.component.ts
+++ src/app/popups/report-form/report-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, Inject } from '@angular/core';
+import { Component, OnInit, Input, Inject, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ValidatorFn, FormControl, FormArray } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { 
-  StoryService 
+  StoryService,
+  TucanValidators,
 } from '../../services';
 
 @Component({
@@ -14,12 +15,18 @@ import {
     '../../../styles/forms.css'
   ]
 })
-export class ReportFormComponent implements OnInit {
-  private reportForm: FormGroup;
-  private storyID: number;
-  public reportSuccess: boolean = false;
+export class ReportFormComponent implements OnInit,OnDestroy {
 
-  offencesList = [
+  private storyID: number;
+
+  public reportForm:FormGroup = null;
+  public initializing:Boolean = true;
+  public checkingValidity:Boolean = false;
+  public submitted:Boolean = false;
+  public reportSuccess:Boolean = false;
+  public reportError:any = null;
+
+  public offencesList = [
     { type: 'Nudity', selected: false, id: 1 },
     { type: 'Violence', selected: false, id: 2 },
     { type: 'Harassment', selected: false, id: 3 },
@@ -31,7 +38,7 @@ export class ReportFormComponent implements OnInit {
   ]
 
   constructor(
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     private _storyService: StoryService,
     public dialogRef: MatDialogRef<ReportFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -40,11 +47,59 @@ export class ReportFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initializeForm();
+  }
+  ngOnDestroy() {
+    this.reportForm = null;
+  }
 
+  initializeForm = () => {
+    
+    this.initializing = true;
+    this.submitted = false;
+    this.checkingValidity = false;
+
+    /*
+    this.reportForm = this.fb.group({
+      offences: new FormArray([], this.minMaxSelectedCheckboxes(1, 1))
+    })
+    */
+
+    this.reportForm = this.fb.group({
+      offence:['',Validators.compose([
+        Validators.required,
+      ])]
+    });
+    this.initializing = false;
+  }
+  _isFormValid = ():Boolean => {
+    return TucanValidators.isFormValid(this.reportForm);
+  }
+  onSubmit = (e:any) => {
+    if (e.preventDefault) e.preventDefault();
+    if (e.stopPropagation) e.stopPropagation();
+    this.checkingValidity = true;
+    this.submitted = true;
+    const valid = this._isFormValid();
+    if (!valid) {
+      this.checkingValidity = false;
+      return;
+    }
+    const selectedID = this.reportForm.get('offence').value;
+    this._storyService.reportStory(this.storyID, selectedID).then(() => {
+      this.reportSuccess = true;
+    }).catch(error=>{
+      this.reportError = error;
+    }).finally(()=>{
+      this.checkingValidity = false;
+    })
+  }
+
+/*  
+  ngOnInit() {
     this.reportForm = this.formBuilder.group({
       offences: new FormArray([], this.minMaxSelectedCheckboxes(1, 1))
     });
-
     this.addCheckboxes();
   }
 
@@ -73,17 +128,16 @@ export class ReportFormComponent implements OnInit {
 
   submit(formValue: any){
     var selectedID: number;
-
     formValue.offences.forEach((offence, index) => {
       if (offence){
         selectedID = this.offencesList[index]['id'];
       }
     });
-    
     this._storyService.reportStory(this.storyID, selectedID).then(() => {
       this.reportSuccess = true;
     })
   }
+  */
 
   closeDialog = () => {
     this.dialogRef.close();
